af0ac9d97922b76ab24fe61cf2217ca407675327
huabin <huabin@users.noreply.github.com>
2025-07-08 09:40:39 +0800

    typo

---
+++ lua/larn/lualine.lua
@@ -0,0 +1,15 @@
+local larn_lualine_theme = {
+  normal = {
+    a = {fg = '#80dfff', bg = '#0e1a22', gui = 'bold'}, 
+    b = {fg = '#a8e6cf', bg = '#1a2a33'},              
+    c = {fg = '#ffd180', bg = '#2c3e4d'},             
+    x = {fg = '#f48fb1', bg = '#2c3e4d'},            
+    y = {fg = '#ffab91', bg = '#1a2a33'},           
+    z = {fg = '#a0cfff', bg = '#1a2a33'},
+  },
+  insert = { a = { bg = "#0e1a22", fg = "#80dfff", gui = "bold" } },
+  visual = { a = { bg = "#3c4b59", fg = "#a8e6cf", gui = "bold" } },
+  replace = { a = { bg = "#1a2a33", fg = "#ffd180", gui = "bold" } },
+}
+
+return larn_lualine_theme
